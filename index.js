03e51293c46e974f92d7187dfc1b8b2be8d4fec4
harishkumark96 <harishkumark05@gmail.com>
2024-04-05 12:10:51 +0530

    15-added login,json data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const amqp = require('amqplib');
 const rabbitmqUrl = process.env.RABBITMQ_URL;
 const cors = require('cors');
+const fs = require('fs');
+const jwt = require('jsonwebtoken')
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
@@ -15,6 +17,12 @@ const io = require('socket.io')(server, {
 });
 let message = '';
 let dataArray =[];
+
+const SECRET_KEY = 'abcdefghi';
+const users = [
+{id:1,username:'admin',pin:'1234'},
+{id:2,username:'user',pin:'3214'}
+  ]
 
 app.use(cors(
 {
@@ -37,8 +45,64 @@ app.get('/test',(req,res)=>{
 
 app.get('/arduinoArr',(req,res) =>{
     res.send(dataArray)
-    console.log(dataArray)
+    // console.log(dataArray)
 })
+app.post('/login', (req,res)=>{
+  const {username,pin}= req.body;
+console.log(username)
+  const user = users.find(x => x.username === username);
+  if(!user || user.pin !== pin){
+
+    return res.status(401).json({message:'Invalid user pin'});
+  }
+  else{
+    const token = jwt.sign({userId:user.id},SECRET_KEY);
+    res.json({token})
+  }
+})
+
+//verify token for auth
+
+const tokenVerification =(req,res,next)=>{
+  const bearerHeader = req.headers['authorization'];
+  if(typeof bearerHeader !== 'undefined'){
+    const tok= bearerHeader.split(' ')[1];
+    req.token = tok;
+    next();
+  }else{
+    res.sendStatus(403);
+  }
+}
+
+app.get('/data',tokenVerification,(req,res)=>{
+  jwt.verify(req.token, SECRET_KEY, (err,authData)=>{
+    if(err){
+      res.sendStatus(403)
+    }else{
+      res.json({data:['item1','item2']})
+    }
+  })
+})
+app.get('/profile/data.json', tokenVerification, (req, res) => {
+    // Verify the token
+    jwt.verify(req.token, SECRET_KEY, (err, authData) => {
+        if (err) {
+            // If token verification fails, return a 403 Forbidden status
+            res.sendStatus(403);
+        } else {
+            // If token verification succeeds, read data from 'data.json' file
+            fs.readFile('./data.json', 'utf8', (err, data) => {
+                if (err) {
+                    res.status(500).send('Internal Server Error');
+                    return;
+                }
+                // Set response headers and send the data
+                res.setHeader('Content-Type', 'application/json');
+                res.send(data);
+            });
+        }
+    });
+});
 async function connect() {
     try {
         const connection = await amqp.connect(rabbitmqUrl);
